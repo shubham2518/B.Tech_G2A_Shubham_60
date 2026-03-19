e3e2aaeb50a9cb8cdc950cc031c23dee5a90c3db
shubham2518 <modanwal2509@gmail.com>
2026-03-19 19:44:54 +0530

    Create Greater of two numbers.c

---
+++ Greater of two numbers.c
@@ -0,0 +1,14 @@
+//WAP TO FIND THE GREATER OF TWO NO. USING CONDITONAL OPERATOR//
+
+#include<stdio.h>
+int main () {
+  int a,b,max;
+
+  printf("Enter two numbers:\n");
+  scanf("%d %d",&a,&b);
+
+  max=a>b? a:b,
+  printf("\ngreater number=%d",max);
+
+  return 0;
+}
