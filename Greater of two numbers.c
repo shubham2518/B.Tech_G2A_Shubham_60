f6cb6bdef6bcde1a98652c70844dc725827152b1
shubham2518 <modanwal2509@gmail.com>
2026-03-19 19:34:38 +0530

    Update Greater of two numbers.c

--- Greater of two numbers.c
+++ Greater of two numbers.c
@@ -1,1 +1,20 @@
-i
+//WAP TO FIND THE GREATER OF THREE NO. USING CONDITONAL OPERATOR//
+
+#include <stdio.h>
+int main() {
+    int a, b, c, max;
+
+    printf("Enter three numbers:\n");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a > b && a > c)
+        max = a;
+    else if (b > a && b > c)
+        max = b;
+    else
+        max = c;
+
+    printf("GREATER NUMBER = %d", max);
+
+    return 0;
+}
