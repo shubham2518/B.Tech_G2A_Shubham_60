bfe3f4dc0265381c548a527ff29d7c3924f35f74
shubham2518 <modanwal2509@gmail.com>
2026-03-19 19:35:39 +0530

    Create Check a number +ve, -ve or zero.c

---
+++ Check a number +ve, -ve or zero.c
@@ -0,0 +1,21 @@
+//WAP TO CHECK A NUMBER +VE -VE OR ZERO //
+
+#include <stdio.h>
+int main (){
+  int a;
+
+  printf("Enter the number [a]:");
+  scanf("%d",&a);
+
+if (a>0)
+ printf("a is positive");
+
+else if(a<0)
+ printf("a is negative");
+
+else if(a=0)
+ printf("a is zero");
+
+ return 0;
+
+}
