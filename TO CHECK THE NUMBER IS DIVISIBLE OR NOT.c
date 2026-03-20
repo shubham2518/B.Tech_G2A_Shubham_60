63c4f60c8769686225e33341db4955f815161093
shubham2518 <modanwal2509@gmail.com>
2026-03-21 00:13:35 +0530

    Add program to check divisibility by 5 and 7

---
+++ TO CHECK THE NUMBER IS DIVISIBLE OR NOT.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+  int n;
+
+  printf("Enter the number : ");
+  scanf("%d",&n);
+
+if (n%5==0 && n%7==0)
+ printf("THE NUMBER IS  DIVISIBLE.");
+
+else 
+ printf("THE NUMBER IS NOT DIVISIBLE.");
+
+
+ return 0;
+
+}
