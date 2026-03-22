b004f5cd2dce1636ae167f6e3c46b239c72eeefd
shubham2518 <modanwal2509@gmail.com>
2026-03-22 23:18:12 +0530

    Add prime number printing program
    Implement a program to print prime numbers up to n.

---
+++ PRINT n PRIME NUMBER.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){                         // n = RANGE LIMIT
+
+  int n, i, j, count;                   // i = outer loop (for each number i from 1 to n
+                                       
+  printf("Enter value of n : ");      // i represents the current number being checked(i=1,2,3,4..n)
+
+  scanf("%d",&n);                   // j(Check divisibility) = inner loop (for each number j from 1 to i) 
+                                          
+  for (i=1; i<=n; i++){               // if i=3 then j=1,2,3
+  count=0;                          // count represents how many times i is divisible
+
+      for (j=1; j<=i; j++){              
+        if (i%j==0){
+          count++;
+        }
+      }
+
+      if (count==2){
+        printf("%d ",i);
+      }
+    }
+  return 0;
+}
