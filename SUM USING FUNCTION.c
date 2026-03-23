94d61c77fffd0b38c93052a229ed99b50e39ac89
shubham2518 <modanwal2509@gmail.com>
2026-03-24 02:31:41 +0530

    Create SUM USING FUNCTION.c

---
+++ SUM USING FUNCTION.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int add(int x, int y){
+  return x + y;
+}
+int main(){
+  int a, b;
+  printf("Enter the 1st number : ");
+  scanf("%d",&a);
+
+  printf("Enter the 2nd number : ");
+  scanf("%d",&b);
+
+  int sum = add(a, b);
+  printf("%d",sum);
+  return 0;
+}
